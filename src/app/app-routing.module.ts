99cf349ddea811ada49ba921ffc3868a1d6a69b5
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-04-13 17:48:23 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // path par defaut
+  {path : '' , redirectTo : 'home', pathMatch : 'full'},
+  {path : 'home', component: HomeComponent},
+  {path : 'product' ,  component : ProductComponent},
+
+// route parametré 
+  {path : 'detail/:id', component: DetailProductComponent},
+  // route Not found
+  {path : '**' , component: NotFoundComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
